6e014fa5db288f36942947e81e92ae62cc5b6b07
AutomationTest2022 <104378408+AutomationTest2022@users.noreply.github.com>
2022-04-27 12:21:30 -0400

    Add files via upload

---
+++ TestNg Project/src/Class1.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+public class Class1 
+{
+
+	@Test
+	public void Login1()
+	 {
+		 System.out.println("Sunday");
+	}
+	
+	
+	@Test
+	public void Login2()
+	 {
+		 System.out.println("Monday");
+	}
+	
+}
---
+++ TestNg Project/src/Class2.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class Class2 
+{
+
+	@Test
+	public void Login3()
+	 {
+		 System.out.println("Tuesday");
+	}
+	
+	@Test
+	public void Login4()
+	 {
+		 System.out.println("Wednesday");
+	}
+	
+}
